e257488a97ceb32308e88fbeca2384265ee583d5
Rahadian Dewandono <gusdewa@gmail.com>
2017-01-14 00:05:58 +0800

    Add projects for DAL and Models

---
+++ src/MultiStores.Server.Models/MaterialManagement/Material.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiStores.Server.Models.MaterialManagement
+{
+    public class Material
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/MultiStores.Server.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MultiStores.Server.Models")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05710f82-9f9c-4bbe-8eb9-bef1aad234b1")]
